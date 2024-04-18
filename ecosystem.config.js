a93fe7d7eb41711b20c29d8a3bff7b9955273ee5
Abdelmuttalib Ahmed <ahmed.invix@gmail.com>
2024-04-18 13:55:50 +0800

    Update VITE_BASE_API_URL in ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,6 @@ export const apps = [
       PM2_SERVE_PATH: "./dist",
       PM2_SERVE_PORT: 8088,
       PM2_SERVE_SPA: "true",
-      VITE_BASE_API_URL: "http://47.97.51.98:6093/api",
       NODE_ENV: "production",
     },
   },
