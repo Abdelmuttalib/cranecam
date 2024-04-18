1e75738fe1a39d6f23a2f7771f72c61cb1bed816
Abdelmuttalib Ahmed <ahmed.invix@gmail.com>
2024-04-18 14:02:47 +0800

    Update PM2 configuration in ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,15 @@
-export const apps = [
-  {
-    name: `cranecam`,
-    script: "serve",
-    env: {
-      PM2_SERVE_PATH: "./dist",
-      PM2_SERVE_PORT: 8088,
-      PM2_SERVE_SPA: "true",
-      NODE_ENV: "production",
+/* eslint-disable no-undef */
+module.exports = {
+  apps: [
+    {
+      name: `cranecam`,
+      script: "serve",
+      env: {
+        PM2_SERVE_PATH: "./dist",
+        PM2_SERVE_PORT: 8088,
+        PM2_SERVE_SPA: "true",
+        NODE_ENV: "production",
+      },
     },
-  },
-];
+  ],
+};
